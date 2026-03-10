7c3d39003df336cf2ee20ec72115479039e7184f
bdvbs4e <danielfarid63@gmail.com>
2026-03-10 10:27:42 -0500

    correccion ruta de admins

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,7 +1,10 @@
 import { Router } from "express"
 import { listar, crear, actualizar, eliminar } from "../controllers/user.controller.js"
+import { requireAuth } from "../middleware/auth.middleware.js"
 
 const router = Router()
+
+router.use(requireAuth)
 
 router.get("/", listar)
 router.post("/", crear)
