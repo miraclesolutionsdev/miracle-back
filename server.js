1021d3f1977e49193d4076751a897be5730417be
bdvbs4e <danielfarid63@gmail.com>
2026-04-01 09:21:20 -0500

    correción server para crear ticket por Wpp

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", origin)
     res.setHeader("Access-Control-Allow-Credentials", "true")
     res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS")
-    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization")
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization, x-api-key")
     res.setHeader("Vary", "Origin")
   }
   if (req.method === "OPTIONS") {
